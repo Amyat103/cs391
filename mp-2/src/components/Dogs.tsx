b3249fd8789bb3df91afaac77a7c2358fd14a459
Amyat103 <zhoudavid7312@gmail.com>
2024-09-28 21:56:41 -0400

    removed status, not using

--- mp-2/src/components/Dogs.tsx
+++ mp-2/src/components/Dogs.tsx
@@ -8,16 +8,13 @@ const AllDogsDiv = styled.div`
   background-color: bisque;
 `;
 
-const DogDiv = styled.div<{ status: string }>`
+const DogDiv = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   max-width: 30%;
   padding: 2%;
   margin: 1%;
-  background-color: ${(props) =>
-    props.status === 'Alive' ? 'darkorange' : 'black'};
-  color: ${(props) => (props.status !== 'Alive' ? 'white' : 'black')};
   border: 3px darkred solid;
   font: italic small-caps bold calc(2px + 1vw) Papyrus, fantasy;
   text-align: center;
@@ -30,7 +27,7 @@ const DogImage = styled.img`
 function Dogs(props: { data: Characters[] }) {
   return (
     <AllDogsDiv>
-      {props.data.map((dog) => (
+      {props.data.map((dog: Characters) => (
         <DogDiv key={dog.id}>
           <h4>{dog.id}</h4>
           <h2>{dog.breed}</h2>
