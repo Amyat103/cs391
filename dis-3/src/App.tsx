ad8929b46793b78ce49cdafbc476f7b2ab4dc39e
Amyat103 <zhoudavid7312@gmail.com>
2024-10-11 21:31:41 -0400

    discussion 5

--- dis-3/src/App.tsx
+++ dis-3/src/App.tsx
@@ -1,9 +1,31 @@
 // import { useState } from 'react';
 import './App.css';
 import ArtworksListContent from './components/artworks-list-content';
+import AboutPage from './components/about';
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from 'react-router-dom';
+import Header from './components/header';
+
+const Root = () => {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path='/' element={<ArtworksListContent />} />
+        <Route path='/about' element={<AboutPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+const router = createBrowserRouter([{ path: '*', Component: Root }]);
 
 function App() {
-  return <ArtworksListContent />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
