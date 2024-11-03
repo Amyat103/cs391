a848de96ad88d720ef80c5057ff546469ad0960e
Amyat103 <zhoudavid7312@gmail.com>
2024-11-03 15:22:19 -0500

    enlarged input and link button

--- mp-4/app/page.tsx
+++ mp-4/app/page.tsx
@@ -9,6 +9,23 @@ const StyledDiv = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 1rem;
+  font-size: 1.5rem;
+`;
+
+const StyledBox = styled.div`
+  border: 1px solid white;
+  display: flex;
+  flex-direction: column;
+  margin: 3rem;
+  padding: 2rem 1rem;
+  gap: 2rem;
+  algin-items: center;
+  jutify-content: center;
+`;
+
+const StyledInput = styled.input`
+  font-size: 1.5rem;
 `;
 
 export default function Home() {
@@ -18,13 +35,15 @@ export default function Home() {
     <StyledDiv>
       <h1> Find the Weather in any city!</h1>
       <p>Enter a city name below to get the current weather</p>
-      <input
-        type='text'
-        value={city}
-        placeholder='City name'
-        onChange={(e) => setCity(e.target.value)}
-      />
-      <Link href={`${city}`}>Get Weather</Link>
+      <StyledBox>
+        <StyledInput
+          type='text'
+          value={city}
+          placeholder='City name'
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <Link href={`${city}`}>Get Weather</Link>
+      </StyledBox>
     </StyledDiv>
   );
 }
