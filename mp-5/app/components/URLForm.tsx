ed8b519701cab980021bd844d7b755ff720a427b
Amyat103 <zhoudavid7312@gmail.com>
2024-11-21 20:29:08 -0500

    add overall strcture for mp

---
+++ mp-5/app/components/URLForm.tsx
@@ -0,0 +1,11 @@
+export default function URLForm() {
+  return (
+    <form>
+      <label>
+        URL
+        <input type='url' />
+      </label>
+      <button>Shorten</button>
+    </form>
+  );
+}
