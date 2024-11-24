97d0218ae3f0ddb9f6966ae91957519f45f6d618
Amyat103 <zhoudavid7312@gmail.com>
2024-11-23 21:26:05 -0700

    change page to use client

--- mp-5/app/[alias]/page.tsx
+++ mp-5/app/[alias]/page.tsx
@@ -1,16 +1,22 @@
-import { redirect } from 'next/navigation';
+'use client';
+import { useEffect } from 'react';
+import { useParams } from 'next/navigation';
 import getLink from '../lib/getLink';
 
-export default async function AliasPage({
-  params,
-}: {
-  params: { alias: string };
-}) {
-  const url = await getLink(params.alias);
+export default function AliasPage() {
+  const { alias } = useParams();
 
-  if (!url) {
-    return <h2>Link cannot be found</h2>;
-  }
+  useEffect(() => {
+    const fetchAndRedirect = async () => {
+      const url = await getLink(alias as string);
+      if (!url) {
+        return <h2>Link error try again</h2>;
+      }
+      window.location.href = url;
+    };
 
-  redirect(url);
+    fetchAndRedirect();
+  }, [alias]);
+
+  return <h2>Linking</h2>;
 }
